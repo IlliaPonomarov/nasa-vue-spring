1f14cb8bd8815b33a9b674e15dd681804f2d0b1a
Illia <chippaxyucac@gmail.com>
2022-07-22 14:40:59 +0200

    init

---
+++ main/java/com/ponomarov/illia/nasa/spring/vue/NasaSpringVueApplication.java
@@ -0,0 +1,13 @@
+package com.ponomarov.illia.nasa.spring.vue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaSpringVueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NasaSpringVueApplication.class, args);
+    }
+
+}
---
+++ test/java/com/ponomarov/illia/nasa/spring/vue/NasaSpringVueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ponomarov.illia.nasa.spring.vue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaSpringVueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
